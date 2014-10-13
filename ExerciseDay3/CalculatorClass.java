ff67f845cf7a79efe5d1aa4c6282b730301ec28f
unknown <scarda01@CSPC088.dcs.bbk.ac.uk>
2014-10-13 19:04:21 +0100

    Developing the main program

--- ExerciseDay3/CalculatorClass.java
+++ ExerciseDay3/CalculatorClass.java
@@ -4,6 +4,15 @@ public class CalculatorClass
 		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
 	public static void main(String[] args)
 	{
+System.out.print("Please write the two numbers involved in the operation:"); 
+String input1 = System.console().readLine();
+String input2 = System.console().readLine();
+double inputvalue1 = Double.parseDouble(input1);
+double inputvalue2 = Double.parseDouble(input2);
+Calculator calc = new Calculator();
+double result = calc.addition(inputvalue1,inputvalue2);
+System.out.println("That's the result " + result );
+
 }}
 
 class Calculator {private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
@@ -12,7 +21,15 @@ class Calculator {private static Scanner scanner=new Scanner(System.in); static{
 		return first+second;
 	}
 	
-	double subtraction,(double first,double second) {
+	double subtraction(double first,double second) {
 				return first-second;
+		}
+	double multiplication(double first,double second) {
+			return first*second;
+	}
+	double division(double first,double second) {
+			return first/second;
 	}
 }
+
+
