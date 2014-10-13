d6dc2f5309683ab295ee8153ffb14e234b14b207
unknown <scarda01@CSPC088.dcs.bbk.ac.uk>
2014-10-13 18:35:34 +0100

    Removing a comma in the names, occured because of a copy&pasting the operation name from the exercise

---
+++ ExerciseDay3/CalculatorClass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CalculatorClass
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+}}
+
+class Calculator {private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	
+	double addition(double first,double second) {
+		return first+second;
+	}
+	
+	double subtraction,(double first,double second) {
+				return first-second;
+	}
+}
