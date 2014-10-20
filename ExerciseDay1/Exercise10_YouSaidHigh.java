9ea5592fa49a4408ac8efff53a689acf08a9f24e
unknown <scarda01@CSPC049.dcs.bbk.ac.uk>
2014-10-20 18:40:40 +0100

    I didn't really get the requirement, I've changed to code accordingly.

--- ExerciseDay1/Exercise10_YouSaidHigh.java
+++ ExerciseDay1/Exercise10_YouSaidHigh.java
@@ -8,6 +8,7 @@ String input;
 int seqnum = 0;
 int prevnum = -5; //I'm using -5 as NULL value as we're accepting just positive values
 boolean consecutive = true;
+boolean direction = true; //true will be increasing series, false decreasing series
 while(seqnum != -1) {
 	System.out.println("Please enter the next positive number of the sequence:"); 
 	input = System.console().readLine();
@@ -16,9 +17,21 @@ while(seqnum != -1) {
 		System.out.print("Please do not enter negative values"); 
 	}
 	else {
-		if(prevnum != -5 && seqnum != -1) {			
-			if(seqnum != (prevnum + 1) && seqnum != (prevnum - 1)) {
-				consecutive = false;
+		if(prevnum != -5) {
+			if(seqnum == (prevnum - 1)) {
+				direction = false;
+			}
+			if(seqnum != -1) {
+				if(!direction) {
+					if(seqnum != (prevnum - 1)) {
+						consecutive = false;
+					}
+				}
+				else {
+					if(seqnum != (prevnum + 1)) {
+						consecutive = false;			
+					}
+				}
 			}
 		}
 		prevnum = seqnum;
