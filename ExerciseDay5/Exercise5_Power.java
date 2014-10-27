ff145cb8c8176fbf57a7757dc1128aaaf277b761
unknown <scarda01@CSPC048.dcs.bbk.ac.uk>
2014-10-27 20:53:45 +0000

    Fixed a bug, added the print of the power

--- ExerciseDay5/Exercise5_Power.java
+++ ExerciseDay5/Exercise5_Power.java
@@ -10,14 +10,17 @@ int base = Integer.parseInt(value);
 System.out.print("Please provide an exponent:");
 String value2 = System.console().readLine();
 int exponent = Integer.parseInt(value2);
-int power = pow(base,exponent);
+System.out.println("Your Power is: " + MathOps2.pow(base,exponent));
 
 }}
 
 class MathOps2 {private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
 	public static int pow(int base,int exp) {
-		if(exp > 1) {
+		if(exp > 0) {
 			return base*pow(base,--exp);
+		}
+		else {
+			return 1;
 		}
 	}
 }
