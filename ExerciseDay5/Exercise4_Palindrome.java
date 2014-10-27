91c073894fc779e6fe305da26021d826412b3917
unknown <scarda01@CSPC048.dcs.bbk.ac.uk>
2014-10-27 19:36:22 +0000

    Palindrome with iteration(from day 3) and recursion(day 5)

---
+++ ExerciseDay5/Exercise4_Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Exercise4_Palindrome
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+System.out.print("Please write your string in order to find out if it is a palindrome:"); 
+String input = System.console().readLine();
+int index = 0;
+boolean isPalindrome = MyStringsOp.PalindromChecker(input,index);
+System.out.println(isPalindrome);
+
+
+}}
+
+class MyStringsOp {private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static boolean PalindromChecker(String mystring,int index) {
+		if(index > (mystring.length() / 2)) {
+			return true;
+		}
+		int rightpart = mystring.length()-index;
+		if(!mystring.substring(index,index+1).equals(mystring.substring(rightpart-1,rightpart))) {
+			return false;
+		}
+		else {
+			return MyStringsOp.PalindromChecker(mystring,++index);
+		}
+	}
+}
