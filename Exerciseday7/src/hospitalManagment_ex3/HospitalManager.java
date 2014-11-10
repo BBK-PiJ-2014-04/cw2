45bc27d0349d4cc0c661d5f126b6d0acc158edd0
unknown <scarda01@CSPC048.dcs.bbk.ac.uk>
2014-11-10 19:41:51 +0000

    Program initialized, now try to go trough the list

---
+++ Exerciseday7/src/hospitalManagment_ex3/HospitalManager.java
@@ -0,0 +1,30 @@
+package hospitalManagment_ex3;
+
+public class HospitalManager {	
+	 public static void main(String[] args) {
+		 Patient philip = new Patient("Philip",50,"Lupus");
+		 Patient patientList = philip;
+		 Patient mark = new Patient("Mark",25,"Broken Leg");
+		 patientList.addPatient(mark);
+		 Patient carlos = new Patient("Carlos",35,"Fever");
+		 patientList.addPatient(carlos);
+		 Patient violet = new Patient("Violet",21,"TBC");
+		 patientList.addPatient(violet);
+		 Patient matilde = new Patient("Matilde",25,"Alcol Poisoning");
+		 patientList.addPatient(matilde);
+		 Patient alexandre = new Patient("Alexandre",34,"STD");
+		 patientList.addPatient(alexandre);
+		 Patient peter = new Patient("Peter",32,"Cluster Headache");
+		 patientList.addPatient(peter);
+		 Patient paul =  new Patient("Paul",45,"STD");
+		 patientList.addPatient(paul);
+		 Patient maria = new Patient("Maria",33,"Broken elbow");
+		 patientList.addPatient(maria);
+		 Patient anthony = new Patient("Anthony",19,"Hepatitis");
+		 patientList.addPatient(anthony);
+		 
+		 patientList.deletePatient(violet);
+		 patientList.deletePatient(paul);
+		 
+	 }
+}
