369fdaca0e6624fbc1f9035b977ecccc2fbbe7f5
unknown <scarda01@CSPC048.dcs.bbk.ac.uk>
2014-11-17 20:18:04 +0000

    Work in progress..

--- cw2/calculator/FractionCalculator.java
+++ cw2/calculator/FractionCalculator.java
@@ -3,19 +3,47 @@ package calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
 
 public class FractionCalculator {
+	
+	private String operator;
+	String[] possibleOperations = {'+','-','*','/'};
+	
 	public static void main(String[] args) {
 		 String input;
+		 Fraction currentvalue = new Fraction(0,1);
+		 System.out.print(currentvalue.toString());
 		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		 
 		 try {
-			 input = br.readLine();
-			 System.out.println(input);
+			 input = currentvalue + br.readLine();
+			 String[] userInput = input.split("-");
+			 for( String singleValue : userInput ) {
+				 currentvalue = evaluate(currentvalue, singleValue);
+			 }
 	      } catch (IOException ioe) {
 	         System.out.println("The input generated an unexpected exception!");
 	         System.exit(1);
 	      }
 		 
+	}
+	
+	public static Fraction evaluate(Fraction fraction, String inputString){
+		if.contains)
+		switch(inputString) {
+			 case "+":
+				 break;
+			 case "-":	
+				 break;
+			 case "*":
+				 break;
+			 case "/":	
+				 break;
+			 default:
+		 }
+		
+		return null;
 	}
 }
