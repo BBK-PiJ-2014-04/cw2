032f8137c0e9336a2325ed3565c32accf5fb7e20
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2014-11-22 20:33:18 +0000

    Test ok! I also added a few tests without implementing the feature.(as from spec)

--- cw2/calculator/FractionCalculatorTest.java
+++ cw2/calculator/FractionCalculatorTest.java
@@ -13,6 +13,17 @@ public class FractionCalculatorTest {
 		
 		//Test the operations
 		System.out.println((testInputSequence("7 + 9 + 12",new Fraction(12,4))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("7 + 9 + 12",new Fraction(28,1))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("7/2 + 9/2 + 12/2",new Fraction(28,2))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("7/5 - 9/2 * 12/2 / 7/9 + 3",new Fraction(-732,35))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("1/-2 3/2 * 7/2 * 1/3",new Fraction(-7,4))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("1/-2 3/2 * 7/2 * 1/3",new Fraction(7,4))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("1/-2 3/2 * -7/2 * 1/3",new Fraction(7,-4))) ? "ok" : "not ok");
+	
+		//Test the absolute and negate (not implemented yet, I want to try the "sure failing" tests
+		System.out.println((testInputSequence("2/-3 neg",new Fraction(2,3))) ? "ok" : "not ok");
+		System.out.println((testInputSequence("1/-2 3/2 * 7/2 * 1/3 neg",new Fraction(-7,4))) ? "ok" : "not ok");
+		
 	}
 	
 	public static boolean testInputSequence(String input,Fraction result)
