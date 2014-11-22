72bfbf9bd2c1edd37fb36657020f3c4bb6181c1e
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2014-11-22 19:43:01 +0000

    Ok, I think I can now create a few test to see if the system will grab the right input and will also use the currentValue correctly

--- cw2/calculator/FractionCalculator.java
+++ cw2/calculator/FractionCalculator.java
@@ -1,43 +1,130 @@
 package calculator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-public class FractionCalculator {
+public class FractionCalculator 
+{
 	
 	private String operator;
-	private String possibleOperations = "+-*/";
-	private String possibleModifiers = "";
+	private String[] possibleOperations = {"+","-","*","/"};
+	private String[] possibleModifiers = {"a","abs","n","neg","c","q"};
+	public Fraction currentValue;
 	
-	public static void main(String[] args) {
-		 String input = "";
-		 Fraction currentvalue = new Fraction(0,1);
-		 Scanner userInput = new Scanner(System.in);
-		 while(!userInput.next().isEmpty()) { // only when you enter literal string 
-			 input += userInput.next();
-		 }
-		 FractionCalculator myCalculator = new FractionCalculator();
-		 currentvalue = myCalculator.evaluate(currentvalue, input);
+	public static void main(String[] args) 
+	{
+		 String input;
+		 Fraction result = null;
+		 Fraction initialValue = null;
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		 try 
+		 {
+			 while(true) 
+			 {
+				 initialValue = result;
+				 input = br.readLine();
+				 FractionCalculator myCalculator = new FractionCalculator();
+				 if(initialValue == null)
+				 {
+					 initialValue = new Fraction(0,1);
+				 }
+				 result = myCalculator.evaluate(initialValue, input);
+			 }
+	      } 
+		 catch (IOException ioe) 
+		 {
+	         System.out.println("The input generated an unexpected exception!");
+	         System.exit(1);
+	      }
+
 	}
 	
 	public Fraction evaluate(Fraction fraction, String inputString){
 		String[] userInput = inputString.split(" ");
-		if(possibleOperations.contains(inputString))
+		for( String singleValue : userInput ) 
 		{
-			if(operator != null) {
-				System.out.println("Your input is an operator, but there's already one in memory! Let's try again!");
-				return new Fraction(0,1);
-			}
-			else
+			if((singleValue.contains("/") && singleValue.split("/").length == 2) || isInteger(singleValue))
 			{
-				operator = inputString;
+				inputFunction(singleValue);
+			}
+			if(Arrays.asList(possibleOperations).contains(singleValue))
+			{
+				if(!setOperator(singleValue)) {
+					return new Fraction(0,1);
+				}
+			}
+			else if(Arrays.asList(possibleModifiers).contains(singleValue))
+			{
 			}
 		}
-		/*else if() {
-				
-		}*/
-		
 		return null;
 	}
+
+	private void inputFunction(String singleValue) 
+	{
+		Fraction interFraction;
+		if(isInteger(singleValue)) 
+		{
+			interFraction = new Fraction(Integer.parseInt(singleValue),1);
+		}
+		else
+		{
+			interFraction = new Fraction(Integer.parseInt(singleValue.split("/")[0]),Integer.parseInt(singleValue.split("/")[1]));
+		}
+		if(operator == null) 
+		{
+			currentValue = interFraction;
+		}
+		else {
+			performOperation(interFraction,operator);
+		}
+	}
+	
+	public boolean setOperator(String operatorEvaluated)
+	{
+		if(operator != null) {
+			System.out.println("Your input is an operator, but there's already one in memory! Let's try again!");
+			return false;
+		}
+		else
+		{
+			operator = operatorEvaluated;
+			return true;
+		}
+	}
+	
+	public Fraction performOperation(Fraction fraction,String operation)
+	{
+		switch(operation) 
+		{
+			case "+":
+				
+				break;
+			case "-":
+				break;
+			case "/":
+				break;
+			case "*":
+				break;
+		}
+		return null;
+	}
+	
+	public static boolean isInteger(String str) {
+	    try 
+	    {
+	        Integer.parseInt(str);
+	        return true;
+	    } 
+	    catch (NumberFormatException nfe) 
+	    {
+	        return false;
+	    }
+	}
+	
 }
